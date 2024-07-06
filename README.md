076aac9f9b4f71416365898dc4a37aa3c89de32e
Robin Couret <couret.robin@protonmail.com>
2024-07-06 20:39:28 +0200

    add models

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
 # Ai extract API
 
-  
-
 ## How to
-
-  
 
 To use this project take care about ;
 
 - docker-compose.yml configuration
-	- > /bin/bash/ docker compose  up 
+	- > /bin/bash/ docker compose  up
 	- or for discret mode
-	- > /bin/bash/ docker compose  up -d  
+	- > /bin/bash/ docker compose  up -d
 
 - .env configuration (db user, name, port, password, etc.)
-
--- app/models/user.ts
+++ app/models/user.ts
@@ -1,8 +1,14 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { HasMany } from '@adonisjs/lucid/types/relations'
+
+import ExtractionPattern from './extraction_pattern.js'
+import ExtractionRecord from './extraction_record.js'
+import Record from './record.js'
+import HtmlDocument from './html_document.js'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -27,4 +33,16 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @hasMany(() => ExtractionPattern)
+  public extractionPatterns: HasMany<typeof ExtractionPattern>
+
+  @hasMany(() => ExtractionRecord)
+  public extractionRecords: HasMany<typeof ExtractionRecord>
+
+  @hasMany(() => Record)
+  public records: HasMany<typeof Record>
+
+  @hasMany(() => HtmlDocument)
+  public htmlDocuments: HasMany<typeof HtmlDocument>
 }
